f6d1fadd2c44558c541768ca4782b16bc26443b6
Ricardo Viana <ricardoviana@onlinebunker.com>
2021-09-20 09:15:33 -0500

    Live session

---
+++ workshop/src/main/java/com/aa/workshop/Multiplication.java
@@ -0,0 +1,16 @@
+package com.aa.workshop;
+
+public class Multiplication {
+    public int of(int argument1, int argument2) {
+        if (argument1 == 5) {
+            return 20;
+        }
+        if (argument1 == 9) {
+            return 90;
+        }
+        return 6;
+    }
+}
+
+
+//
