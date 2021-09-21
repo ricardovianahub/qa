faaf459e7084d0fae7c7c3c406034e4d75e61a7c
Ricardo Viana <ricardoviana@onlinebunker.com>
2021-09-21 09:32:41 -0500

    Rot13

--- sri/src/main/java/com/aa/sri/pingpong/Factorial.java
+++ sri/src/main/java/com/aa/sri/pingpong/Factorial.java
@@ -1,22 +1,30 @@
 package com.aa.sri.pingpong;
 
 public class Factorial {
-    public static int of(int number) {
-        if (number == 4) {
-            return 24;
+    public static int of_original(int number) {
+        if (number == 1) {
+            return 1;
         }
-        if (number == 3) {
-            return 6;
-        }
-        if (number == 2) {
-            return 2;
-        }
-        int temp = number-1;
-        do{
-            number = number*temp;
-            temp = temp -1;
-        }while(temp>1);
+        int temp = number - 1;
+        do {
+            number = number * temp;
+            temp = temp - 1;
+        } while (temp > 1);
         return number;
-
     }
+
+    public static int of_for(int number) {
+        int result = 1;
+        for (int i = number; i > 1; i--) {
+            result = result * i;
+        }
+        return result;
+    }
+
+    public static int of(int number)
+    {
+        number = of(number-1);
+        return number;
+    }
+
 }
---
+++ sri/src/test/java/com/aa/sri/pingpong/PP003Test.java
@@ -0,0 +1,28 @@
+package com.aa.sri.pingpong;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Test;
+
+public class PP003Test {
+
+    // Implement ROT-13 encoding - method receives one String, return ROT-13 encoded String (ALL CAPS, letters only)
+    // ABCDEFGHIJKLMNOPQRSTWXYZ
+
+    @Test
+    void rotateA() {
+        String argument = "A";
+        String expected = "N";
+
+        assertEquals(expected, Rot13.encode(argument));
+    }
+
+    @Test
+    void rotateWrongCharacter() {
+        assertThrows(IllegalArgumentException.class,
+                () -> Rot13.encode("%")
+        );
+    }
+
+}
