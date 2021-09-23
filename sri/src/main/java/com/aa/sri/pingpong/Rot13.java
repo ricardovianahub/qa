5abc7956244021f25486e77833b56b1b92446c6d
Ricardo Viana <ricardoviana@onlinebunker.com>
2021-09-23 09:40:34 -0500

    Continue ROT-13

--- sri/src/main/java/com/aa/sri/pingpong/Rot13.java
+++ sri/src/main/java/com/aa/sri/pingpong/Rot13.java
@@ -3,17 +3,13 @@ package com.aa.sri.pingpong;
 public class Rot13 {
 
     public static String encode(String argument) throws IllegalArgumentException {
-        try {
-            if (argument.equals("B")) {
-                return "O";
-            }
+        guardEncode(argument);
+        return String.valueOf((char) (argument.charAt(0) + 13));
+    }
 
-            return "N";
-        } catch (Exception exception) {
-            exception.printStackTrace();
+    private static void guardEncode(String argument) {
+        if ("%".equals(argument)) {
+            throw new IllegalArgumentException();
         }
     }
 }
-
-
-
--- sri/src/test/java/com/aa/sri/pingpong/PP003Test.java
+++ sri/src/test/java/com/aa/sri/pingpong/PP003Test.java
@@ -4,26 +4,23 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class PP003Test {
 
     // Implement ROT-13 encoding - method receives one String, return ROT-13 encoded String (ALL CAPS, letters only)
     // ABCDEFGHIJKLMNOPQRSTWXYZ
 
-    @Test
-    void rotateA() {
-        String argument = "A";
-        String expected = "N";
-
+    @ParameterizedTest
+    @CsvSource({
+            "A,N", "B,O", "C,P", "M,Z", // first 13
+            "N,A"
+    })
+    void rotate(String argument, String expected) {
         assertEquals(expected, Rot13.encode(argument));
     }
-    @Test
-    void rotateB() {
-        String argument = "B";
-        String expected = "O";
 
-        assertEquals(expected, Rot13.encode(argument));
-    }
     @Test
     void rotateWrongCharacter() {
         assertThrows(IllegalArgumentException.class,
