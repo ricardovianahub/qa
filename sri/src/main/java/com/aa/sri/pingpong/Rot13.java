6de2de37d519022368a7e94b91f169568af3b671
sri satya <sri.satya.chundru@aa.com>
2021-09-27 23:04:06 -0500

    revised logic using term operations

--- sri/src/main/java/com/aa/sri/pingpong/Rot13.java
+++ sri/src/main/java/com/aa/sri/pingpong/Rot13.java
@@ -13,10 +13,13 @@ public class Rot13 {
     private static String checkEachCharacter(String argument) {
         char arg = argument.charAt(0);
         guardEncode(arg);
-        if ((char)(arg)=='Z'){
-            return String.valueOf( (char) (arg-25));
-        }
-        return String.valueOf((char)(arg+1));
+        return String.valueOf(
+                (char) (arg+ ((arg=='Z')?-25:1))
+        );
+//        if ((char)(arg)=='Z'){
+//            return String.valueOf( (char) (arg-25));
+//        }
+//        return String.valueOf((char)(arg+1));
 //        return String.valueOf(
 //                (char) (arg + ((arg >= 'N') ? -13 : 13))
 //        );
