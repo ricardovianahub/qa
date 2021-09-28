f8fda5d37a31c6b0299b4fe820dd7dff3e7fc598
sri satya <sri.satya.chundru@aa.com>
2021-09-27 22:21:30 -0500

    updated params

--- sri/src/test/java/com/aa/sri/pingpong/PP004Test.java
+++ sri/src/test/java/com/aa/sri/pingpong/PP004Test.java
@@ -1,10 +1,40 @@
 package com.aa.sri.pingpong;
 
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
 public class PP004Test {
 
     // Implement Slide 1 encodingg - method receives one String, returns String where all
     // characters shift +1 in the alphabet, and Z goes back to A - all CAPS
     // Example: ABC = BCD; XYZ - YZA
+    @ParameterizedTest
+    @CsvSource({
+//            "A,N", "B,O", "C,P", "M,Z", // first 13
+//            "N,A", "O,B", "P,C", "Z,M",
+//            "ABC,NOP",
+//            "BCM,OPZ"
+            "ABC,BCD",
+            "XYZ,YZA"
+    })
+    void rotate(String argument, String expected) {
+        assertEquals(expected, Rot13.encode(argument));
+    }
 
+    @ParameterizedTest
+    @CsvSource({
+            "%", "[", "]", "\\#", "!", "{"
+    })
+    void rotateWrongCharacter(String badCharacter) {
+        assertThrows(IllegalArgumentException.class,
+                () -> Rot13.encode(badCharacter)
+        );
+    }
+
+    @Test
+    void getsExceptionObject() {
+        Exception actual = Rot13.example();
+        assertEquals(IllegalArgumentException.class, actual.getClass());
+    }
 
 }
