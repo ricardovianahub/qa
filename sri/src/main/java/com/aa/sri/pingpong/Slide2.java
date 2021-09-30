f132c0731ead7262befcf136fb129bd1f73e6a3a
srisatya86 <sri.satya.chundru@aa.com>
2021-09-30 07:27:24 -0500

    added test for slide2 requirement

---
+++ sri/src/main/java/com/aa/sri/pingpong/Slide2.java
@@ -0,0 +1,14 @@
+package com.aa.sri.pingpong;
+
+public class Slide2 extends CharacterEncoder {
+
+    @Override
+    String checkEachCharacter(String argument) {
+        char arg = argument.charAt(0);
+        guardEncode(arg);
+        return String.valueOf(
+                (char) (arg- ((arg=='A')?+25:1))
+        );
+    }
+
+}
--- sri/src/test/java/com/aa/sri/pingpong/PP005Test.java
+++ sri/src/test/java/com/aa/sri/pingpong/PP005Test.java
@@ -1,9 +1,45 @@
 package com.aa.sri.pingpong;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class PP005Test {
 
-    // Implement Slide 2 encodingg - method receives one String, returns String where all
+    private Slide2 slide2;
+
+    @BeforeEach
+    void setup() {
+        slide2 = new Slide2();
+    }
+    @ParameterizedTest
+    @CsvSource({
+            "ABC,ZAB",
+            "XYZ,WXY"
+    })
+    void rotate(String argument, String expected) {
+        assertEquals(expected, slide2.encode(argument));
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "%", "[", "]", "\\#", "!", "{"
+    })
+    void rotateWrongCharacter(String badCharacter) {
+        assertThrows(IllegalArgumentException.class,
+                () -> slide2.encode(badCharacter)
+        );
+    }
+
+}
+
+
+
+
+// Implement Slide 2 encodingg - method receives one String, returns String where all
     // characters shift -1 in the alphabet, and A goes back to Z - all CAPS
     // Example: ABC = ZAB; XYZ - WXY
 
-}
