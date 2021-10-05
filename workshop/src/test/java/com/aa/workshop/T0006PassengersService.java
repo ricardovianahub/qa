73ecfbf91c81faefdbdae4b8b62bf3bb79779cc9
Ricardo Viana <ricardoviana@onlinebunker.com>
2021-10-05 08:57:26 -0500

    Additional T0006PassengersService exercise

--- workshop/src/test/java/com/aa/workshop/T0006PassengersService.java
+++ workshop/src/test/java/com/aa/workshop/T0006PassengersService.java
@@ -33,4 +33,20 @@ public class T0006PassengersService {
         assertEquals(15, lines.size());
     }
 
+    // Take every odd row from the list of passengers and concatenate all origination airport codes and return it
+
+    @Test
+    void oddRowsOfPassengersConcatenateOriginationAirport() throws JsonProcessingException {
+        String response = testRestTemplate.getForObject("http://ricbox.com/passengers", String.class);
+        List<Map> lines = objectMapper.readValue(response, List.class);
+        int[] validNumbers = new int[] {1,3,5,7,9};
+        for (Map map : lines) {
+//            if () {
+//
+//            }
+            map.get("origination");
+        }
+    }
+
+
 }
