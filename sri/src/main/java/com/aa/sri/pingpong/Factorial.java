37757be1e71069f9acfa07f2c866f6dc5d0251f8
Ricardo Viana <ricardoviana@onlinebunker.com>
2021-10-12 09:22:39 -0500

    factorial

--- sri/src/main/java/com/aa/sri/pingpong/Factorial.java
+++ sri/src/main/java/com/aa/sri/pingpong/Factorial.java
@@ -1,31 +1,12 @@
 package com.aa.sri.pingpong;
 
 public class Factorial {
-  /*  public static int of_original(int number) {
-        if (number == 1) {
-            return 1;
-        }
-        int temp = number - 1;
-        do {
-            number = number * temp;
-            temp = temp - 1;
-        } while (temp > 1);
-        return number;
-    }
-
-    public static int of_for(int number) {
-        int result = 1;
-        for (int i = number; i > 1; i--) {
-            result = result * i;
-        }
-        return result;
-    }*/
 
     public static int of(int number) {
-        if (number == 0) {
+        if (number <= 1) {
             return 1;
         }
-        number = number*of(number - 1);
+        number = number * of(number - 1);
         return number;
     }
 
