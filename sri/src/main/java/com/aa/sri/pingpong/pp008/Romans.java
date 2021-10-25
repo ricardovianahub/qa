7e7356eacb90c6a4a3a1e3627600803d1f39a10b
Ricardo Viana <ricardoviana@onlinebunker.com>
2021-10-25 09:30:21 -0500

    RomansTest - started story 2

--- sri/src/main/java/com/aa/sri/pingpong/pp008/Romans.java
+++ sri/src/main/java/com/aa/sri/pingpong/pp008/Romans.java
@@ -4,33 +4,20 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Romans {
+    Map<Integer, String> romanNumerals = new HashMap<>() {{
+        put(1,"I");
+        put(5,"V");
+        put(10,"X");
+        put(50,"L");
+        put(100,"C");
+        put(500,"D");
+        put(1000,"M");
+    }};
+
     public String convert(int argument) {
-        Map<Integer, String> romanNumerals = new HashMap<>();
-        romanNumerals.put(1,"I");
-        romanNumerals.put(5,"V");
-        romanNumerals.put(10,"X");
-        romanNumerals.put(50,"L");
-        romanNumerals.put(100,"C");
-        romanNumerals.put(500,"D");
-        romanNumerals.put(1000,"M");
-        if (argument == 5) {
-            return romanNumerals.get(5);
+        if (argument == 2) {
+            return "II";
         }
-        if (argument == 10) {
-            return romanNumerals.get(10);
-        }
-        if (argument == 50) {
-            return romanNumerals.get(50);
-        }
-        if (argument == 100) {
-            return romanNumerals.get(100);
-        }
-        if (argument == 500) {
-            return romanNumerals.get(500);
-        }
-        if (argument == 1000) {
-            return romanNumerals.get(1000);
-        }
-        return romanNumerals.get(1);        
+        return romanNumerals.get(argument);
     }
 }
--- sri/src/test/java/com/aa/sri/pingpong/pp008/RomansTest.java
+++ sri/src/test/java/com/aa/sri/pingpong/pp008/RomansTest.java
@@ -17,4 +17,15 @@ public class RomansTest {
         assertEquals(expected, actual);
     }
 
+    @ParameterizedTest
+    @CsvSource({"2,II"})
+    void multipleLetters_story2(int argument, String expected) {
+        // setup
+        Romans romans = new Romans(); // all things being equal, instantiate
+        // execution
+        String actual = romans.convert(argument);
+        // assert
+        assertEquals(expected, actual);
+    }
+
 }
