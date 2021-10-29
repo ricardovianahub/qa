31044fb46b212d3f1f66a993c54511e7cfedb419
srisatya86 <sri.satya.chundru@aa.com>
2021-10-29 09:37:44 -0500

    updated Romans code

--- sri/src/main/java/com/aa/sri/pingpong/pp008/Romans.java
+++ sri/src/main/java/com/aa/sri/pingpong/pp008/Romans.java
@@ -1,23 +1,56 @@
 package com.aa.sri.pingpong.pp008;
 
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class Romans {
-    Map<Integer, String> romanNumerals = new HashMap<>() {{
-        put(1,"I");
-        put(5,"V");
-        put(10,"X");
-        put(50,"L");
-        put(100,"C");
-        put(500,"D");
-        put(1000,"M");
+    Map<Integer, String> romanNumerals = new LinkedHashMap<>() {{
+        put(1000, "M");
+        put(500, "D");
+        put(100, "C");
+        put(50, "L");
+        put(10, "X");
+        put(5, "V");
+        put(1, "I");
     }};
 
     public String convert(int argument) {
-        if (argument == 2) {
-            return "II";
+        String result;
+        result = repeatedLetters(argument, 1, 3);
+        if (result != null) {
+            return result;
+        }
+        result = repeatedLetters(argument, 10, 30);
+        if (result != null) {
+            return result;
+        }
+        result = repeatedLetters(argument, 100, 300);
+        if (result != null) {
+            return result;
+        }
+        result = repeatedLetters(argument, 1000, 3000);
+        if (result != null) {
+            return result;
+        }
+        if (argument == 6||argument == 11) {
+            StringBuilder sb = new StringBuilder();
+            System.out.println(romanNumerals.entrySet().toString());
+            for(Map.Entry<Integer,String> entry: romanNumerals.entrySet()){
+                if(entry.getKey()<= argument){
+                    sb.append(entry.getValue());
+
+                }
+            }
+            return sb.toString();
         }
         return romanNumerals.get(argument);
+    }
+
+    private String repeatedLetters(int argument, int lowerRange, int higherRange) {
+        if (argument >= lowerRange && argument <= higherRange) {
+            return romanNumerals.get(lowerRange).repeat(argument / lowerRange);
+        }
+        return null;
     }
 }
--- sri/src/test/java/com/aa/sri/pingpong/pp008/RomansTest.java
+++ sri/src/test/java/com/aa/sri/pingpong/pp008/RomansTest.java
@@ -18,7 +18,7 @@ public class RomansTest {
     }
 
     @ParameterizedTest
-    @CsvSource({"2,II"})
+    @CsvSource({"2,II","3,III","20,XX","30,XXX","200,CC","300,CCC","2000,MM","3000,MMM"})
     void multipleLetters_story2(int argument, String expected) {
         // setup
         Romans romans = new Romans(); // all things being equal, instantiate
@@ -27,5 +27,14 @@ public class RomansTest {
         // assert
         assertEquals(expected, actual);
     }
-
+    @ParameterizedTest
+    @CsvSource({"6,VI","11,XI"})
+    void multipleLetters_story3(int argument, String expected) {
+        // setup
+        Romans romans = new Romans(); // all things being equal, instantiate
+        // execution
+        String actual = romans.convert(argument);
+        // assert
+        assertEquals(expected, actual);
+    }
 }
